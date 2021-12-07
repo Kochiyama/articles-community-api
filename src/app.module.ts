7900e0223a4366ba63580bf40bf57374ef7d7176
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-07 12:04:16 -0400

    Fix: bad import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './authentication/auth.module';
-import { ArticleModule } from './article/article.module';
+import { ArticleModule } from './modules/article/article.module';
 
 @Module({
   imports: [UserModule, AuthModule, ArticleModule],
