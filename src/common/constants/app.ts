79a48c1bade076d0387307bfc0d6a8ae37df6c58
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-08 12:18:43 -0400

    Chore: setup for heroku deploy

---
+++ src/common/constants/app.ts
@@ -0,0 +1,3 @@
+export const appConstants = {
+  port: process.env.PORT,
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { appConstants } from './common/constants/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(appConstants.port || 3000);
 }
 bootstrap();
