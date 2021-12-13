2b52a63d882cb42b69afba5ac35175902e2b4f90
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-12 20:30:20 -0400

    Chore: add test domain to cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://articles-community-web.vercel.app',
+    ],
   });
 
   app.useGlobalPipes(
